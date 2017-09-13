14f6d4be96f06edfef6e46b6565ce9f9f49b730d
Romain <romain.brette@inserm.fr>
2017-09-13 16:40:05 +0200

    Test GUI displays clicked coordinates wrt center

--- testgui.py
+++ testgui.py
@@ -4,15 +4,16 @@ This is a test GUI, to test the functionality
 from devices import *
 from vision import *
 from gui import *
-from threading import Thread
 import cv2
 
 def callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print x, y
+        xs = x-camera.width/2
+        ys = y-camera.height/2
+        print xs, ys
 
 camera = OpenCVCamera()
-livefeed = LiveFeed(camera, mouse_callback=callback) # callback doesn't work, why?
+livefeed = LiveFeed(camera, mouse_callback=callback)
 
 cv2.waitKey(0)
 
