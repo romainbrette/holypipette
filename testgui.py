5724c61e51b9ad9f9a5acb44d1b3069d9dfd0677
Romain <romain.brette@inserm.fr>
2017-09-13 13:57:54 +0200

    Tried mouse callback; doesn't work

--- testgui.py
+++ testgui.py
@@ -5,8 +5,11 @@ from devices import *
 from vision import *
 from gui import *
 
+def callback(event, x, y, flags, param):
+    print x, y
+
 camera = OpenCVCamera()
-livefeed = LiveFeed(camera)
+livefeed = LiveFeed(camera, mouse_callback=callback) # callback doesn't work, why?
 
 raw_input("Press Enter to continue...")
 
