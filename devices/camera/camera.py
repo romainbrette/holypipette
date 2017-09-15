8564da35180684b9c3889063d5365572882fa3db
Romain <romain.brette@inserm.fr>
2017-09-15 15:43:15 +0200

    snap_center removed; replaced by crop_center in vision/

--- devices/camera/camera.py
+++ devices/camera/camera.py
@@ -24,17 +24,3 @@ class Camera(object):
         Returns the current image
         '''
         return None
-
-    def snap_center(self, ratio = 32):
-        '''
-        Returns the center of the image.
-
-        Parameters
-        ----------
-        ratio : size ratio of cropped image to original image
-        '''
-        image = self.snap()
-        shape = [self.height, self.width]
-        return image[shape[0] / 2 - 3 * shape[0] / ratio:shape[0] / 2 + 3 * shape[0] / ratio,
-                     shape[1] / 2 - 3 * shape[1] / ratio:shape[1] / 2 + 3 * shape[1] / ratio]
-
