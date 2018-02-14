271453821bd1afc631e255229d8f1bb84bd8bb60
Marcel Stimberg <marcel.stimberg@inserm.fr>
2018-02-14 17:09:16 +0100

    Add a "fake setup" for GUI development (no hardware access required)

---
+++ configuration/setup_fake_rig.py
@@ -0,0 +1,14 @@
+'''
+"Fake setup" for GUI development on a computer without access to a rig
+'''
+from devices.camera.camera import FakeCamera
+from devices.manipulator import *
+
+camera = FakeCamera()
+controller = FakeManipulator()
+stage = ManipulatorUnit(controller, [7, 8])
+microscope = Microscope(controller, 9)
+calibrated_stage = CalibratedStage(stage, None, microscope, camera=camera)
+units = [ManipulatorUnit(controller, [1, 2, 3]), ManipulatorUnit(controller, [4, 5, 6])]
+calibrated_units = [CalibratedUnit(units[0], calibrated_stage, microscope, camera=camera),\
+                    CalibratedUnit(units[1], calibrated_stage, microscope, camera=camera)]
