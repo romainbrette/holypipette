bc042e33b095780e7830329db99fb910e7f3eafd
huyhoang93 <36202907+huyhoang93@users.noreply.github.com>
2018-04-23 18:20:19 +0200

    Testing Code

--- holypipette/controller/patch.py
+++ holypipette/controller/patch.py
@@ -1,9 +1,12 @@
 import time
 
 import numpy as np
+from numpy import array
+from math import atan2
+import cv2
 
 from .base import TaskController
-
+from setup_script import *
 
 def MatrixCalculation(n):
     i =1;
@@ -11,6 +14,45 @@ def MatrixCalculation(n):
         i=i+1
     return i
 
+def moveto(x, y):
+    xs = x-camera.width/2
+    ys = y-camera.height/2
+    #print xs, ys
+    calibrated_stage.reference_move(calibrated_stage.reference_position()-array([xs, ys, 0]))
+
+def where_is_paramecium(frame): # Locate paramecium
+    height, width = frame.shape[:2]
+    ratio = width/256
+    resized = cv2.resize(frame, (width/ratio, height/ratio))
+    gauss = cv2.GaussianBlur(resized, (9, 9), 0)
+    canny = cv2.Canny(gauss, gauss.shape[0]/8, gauss.shape[0]/8)
+    ret, thresh = cv2.threshold(canny, 127, 255, 0)
+    ret = cv2.findContours(thresh, 1, 2)
+    contours, hierarchy = ret[-2], ret[-1] # for compatibility with opencv2 and 3
+    distmin = 1e6
+    for cnt in contours:
+        try:
+            M = cv2.moments(cnt)
+            if (cv2.arcLength(cnt, True) > 90) & bool(M['m00']):
+                (x, y), radius = cv2.minEnclosingCircle(cnt)
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+                u20 = int(M['m20']/M['m00'] - cx**2)
+                u02 = int(M['m02'] / M['m00'] - cy ** 2)
+                u11 = int(M['m11'] / M['m00'] - cx * cy)
+                theta = atan2((u20-u02), 2*u11)/2
+                radius = int(radius)
+                dist = ((x - width/2) ** 2 + (y - height/2) ** 2) ** 0.5
+                if (radius < 55) & (radius > 25) & (dist<distmin):
+                    distmin=dist
+                    xmin, ymin =x, y
+                    angle = theta # not used here
+        except cv2.error:
+            pass
+    if distmin<1e5:
+        return xmin*ratio,ymin*ratio
+    else:
+        return None,None
 
 class AutopatchError(Exception):
     def __init__(self, message = 'Automatic patching error'):
@@ -301,8 +343,11 @@ class AutoPatcher(TaskController):
         try:
             i = 0
             start_position = self.calibrated_unit.position()
+            z0 = self.microscope.position()
 
             # Move the pipette to the paramecium tank.
+            self.microscope.absolute_move(0)
+            self.microscope.wait_until_still()
             self.calibrated_unit.absolute_move(self.paramecium_tank_position[0], 0)
             self.calibrated_unit.wait_until_still(0)
             self.calibrated_unit.absolute_move(self.paramecium_tank_position[2] - 5000, 2)
@@ -353,9 +398,13 @@ class AutoPatcher(TaskController):
             self.calibrated_unit.wait_until_still(2)
             self.calibrated_unit.absolute_move(start_position[0], 0)
             self.calibrated_unit.wait_until_still(0)
+            self.microscope.absolute_move(z0)
+            calibrated_stage.relative_move(1000, axis=0)
+            calibrated_stage.wait_until_still()
+            calibrated_stage.relative_move(1000, axis=1)
+            calibrated_stage.wait_until_still()
 
             #Paramecium hunting
-
 
 
 
