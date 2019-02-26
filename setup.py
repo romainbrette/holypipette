aba98b8cef4bf18d3e66f94a4368b355bc8c0011
Marcel Stimberg <marcel.stimberg@inserm.fr>
2019-02-26 16:00:57 +0100

    Add basic setup.py file so it can be installed as package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holypipette',
+    version='0.1',
+    description='Semi-automated patch clamp recordings',
+    url='https://github.com/romainbrette/holypipette/',
+    author='Romain Brette, Marcel Stimberg, Hoang Nguyen',
+    author_email='romain.brette@inserm.fr',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    install_requires=['numpy', 'PyQt5']
+)
