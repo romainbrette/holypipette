e9dd5e9b64fc1e1cbecae1d11cabc105a0c995ff
romainbrette <romain.brette@inserm.fr>
2021-06-11 15:08:53 +0200

    Simplified Paramecium GUI

---
+++ holypipette/gui/paramecium_device2.py
@@ -0,0 +1,69 @@
+'''
+Simplified Paramecium device GUI
+
+GUI for Paramecium electrophysiology, with the immobilization device
+'''
+from __future__ import absolute_import
+
+from PyQt5.QtCore import Qt
+from holypipette.gui.manipulator import ManipulatorGui
+
+#################################################
+# Paramecium experiment with the device method #
+#################################################
+class ParameciumDeviceGui(ManipulatorGui):
+
+    def __init__(self, stage, microscope, camera, units, config_filename='paramecium_device.cfg'):
+        super(ParameciumDeviceGui, self).__init__(camera,
+                                            pipette_interface=ParameciumDeviceSimplifiedInterface(stage, microscope, camera, units, config_filename=config_filename))
+        self.display_edit_funcs.append(self.display_timer)
+        self.setWindowTitle("Paramecium device")
+        self.add_config_gui(self.interface.config)
+
+    def register_commands(self):
+        # We have all the commandes of the pipettes interface
+        super(ParameciumDeviceGui, self).register_commands(manipulator_keys = False)
+
+        #self.register_mouse_action(Qt.LeftButton, Qt.ShiftModifier,
+        #                           self.interface.move_pipette_floor) # move to floor level, x axis last
+        #self.register_mouse_action(Qt.LeftButton, Qt.ControlModifier,
+        #                           self.interface.move_pipette_working_level) # move above clicked position
+
+        self.register_key_action(Qt.Key_Space, None,
+                                 self.interface.move_pipette_down) # move to impalement level
+        self.register_key_action(Qt.Key_0, None,
+                                 self.interface.reset_timer)
+        self.register_key_action(Qt.Key_Less, None,
+                                 self.interface.focus_working_level)
+        self.register_key_action(Qt.Key_Greater, None,
+                                 self.interface.focus_calibration_level)
+        #self.register_key_action(Qt.Key_At, None,
+        #                         self.interface.set_center)
+        #self.register_key_action(Qt.Key_Home, None,
+        #                         self.interface.move_to_center)
+        self.register_key_action(Qt.Key_Space, Qt.ControlModifier,
+                                 self.interface.move_pipette_in)
+        self.register_key_action(Qt.Key_W, None,
+                                 self.interface.partial_withdraw)
+        self.register_key_action(Qt.Key_Space, Qt.ShiftModifier,
+                                 self.interface.move_pipette_until_drop)
+
+if __name__ == '__main__':
+    import sys
+
+    from PyQt5 import QtWidgets
+
+    from holypipette.log_utils import console_logger
+    from holypipette.interface.paramecium_device import ParameciumDeviceSimplifiedInterface
+
+    from setup_script import *
+
+    console_logger()  # Log to the standard console as well
+
+    app = QtWidgets.QApplication(sys.argv)
+    gui = ParameciumDeviceGui(stage, microscope, camera, units, config_filename='paramecium_device.cfg')
+    gui.initialize()
+    gui.show()
+    ret = app.exec_()
+
+    sys.exit(ret)
--- holypipette/interface/paramecium_device.py
+++ holypipette/interface/paramecium_device.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from holypipette.config import Config, NumberWithUnit, Number, Boolean
-from holypipette.interface import TaskInterface, command, blocking_command
+from holypipette.interface import TaskInterface, command, blocking_command, PipetteInterface
 from holypipette.vision import cardinal_points
 from holypipette.controller.paramecium_device import ParameciumDeviceController
 import os
@@ -50,12 +50,81 @@ class ParameciumDeviceInterface(TaskInterface):
         self.camera = camera
         self.calibrated_unit = CalibratedUnitProxy(pipette_interface)
         self.calibrated_units = pipette_interface.calibrated_units
-        self.previous_shift_click = None
-        self.shift_click_time = time.time()-1e6 # a long time ago
 
         self.controller = ParameciumDeviceController(self.calibrated_unit,
                                                pipette_interface.microscope,
                                                pipette_interface.calibrated_stage,
+                                               camera,
+                                               self.config)
+
+    @command(category='Paramecium',
+                     description='Focus on working level')
+    def focus_working_level(self):
+        self.controller.microscope.absolute_move(self.controller.microscope.floor_Z + self.config.working_level*self.controller.microscope.up_direction)
+
+    @command(category='Paramecium',
+                     description='Focus on calibration level')
+    def focus_calibration_level(self):
+        self.controller.microscope.absolute_move(self.controller.microscope.floor_Z + self.config.calibration_level*self.controller.microscope.up_direction)
+
+    @blocking_command(category='Paramecium',
+                     description='Move pipette down to position at working distance level',
+                     task_description='Moving pipette to position at working distance level')
+    def move_pipette_working_level(self, xy_position):
+        x, y = xy_position
+        position = np.array([x, y, self.controller.microscope.floor_Z + self.config.working_level*self.controller.microscope.up_direction])
+        self.debug('asking for safe move to {}'.format(position))
+        self.execute(self.controller.calibrated_unit.safe_move, argument=position)
+
+    @blocking_command(category='Paramecium',
+                     description='Move pipette vertically to impalement level',
+                     task_description='Moving pipette vertically to impalement level')
+    def move_pipette_down(self):
+        x, y, _ = self.controller.calibrated_unit.reference_position()
+        position = np.array([x, y, self.controller.microscope.floor_Z + self.config.impalement_level*self.controller.microscope.up_direction])
+        self.debug('asking for move to {}'.format(position))
+        self.execute(self.controller.calibrated_unit.reference_move, argument=position)
+
+    @blocking_command(category='Paramecium',
+                      description='Partially withdraw the pipette',
+                      task_description='Withdrawing the pipette')
+    def partial_withdraw(self):
+        self.execute(self.controller.partial_withdraw)
+
+    @blocking_command(category='Paramecium',
+                      description='Move pipette to impalement level by a side move',
+                      task_description='Moving pipette to impalement level by a side move')
+    def move_pipette_in(self):
+        self.execute(self.controller.move_pipette_in)
+
+    @blocking_command(category='Paramecium',
+                      description='Move pipette down until potential drop',
+                      task_description='Moving pipette down until potential drop')
+    def move_pipette_until_drop(self):
+        self.execute(self.controller.move_pipette_until_drop)
+
+    #@blocking_command(category='Paramecium',
+    #                  description='Partially withdraw the pipette',
+    #                  task_description='Withdrawing the pipette')
+    #def set_center(self):
+        #self.center = self.cali
+
+        #self.register_key_action(Qt.Key_At, None,
+        #                         self.paramecium_interface.set_center)
+        #self.register_key_action(Qt.Key_Home, None,
+        #                         self.paramecium_interface.move_to_center)
+
+
+class ParameciumDeviceSimplifiedInterface(PipetteInterface):
+
+    def __init__(self, stage, microscope, camera, units,
+                 config_filename=None):
+        super(ParameciumDeviceSimplifiedInterface, self).__init__(stage, microscope, camera, units,config_filename)
+        self.config = ParameciumDeviceConfig(name='Paramecium')
+
+        self.controller = ParameciumDeviceController(self.calibrated_unit,
+                                               microscope,
+                                               self.calibrated_stage,
                                                camera,
                                                self.config)
 
