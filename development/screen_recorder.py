4e37ef4b09574c375563bc02d454d8e37a48468e
romainbrette <romain.brette@inserm.fr>
2022-03-30 11:48:50 +0200

    Screen recorder

---
+++ development/screen_recorder.py
@@ -0,0 +1,61 @@
+'''
+Records camera from screen capture.
+
+Problem: the clock may change, this causes a localization issue.
+
+Screenshot: 0.47 s ! 0.42 with pyautogui
+'''
+import imageio
+import time
+import numpy as np
+import pyautogui
+
+t=time.time()
+for _ in range(10):
+    image = pyautogui.screenshot()
+print(time.time()-t)
+exit(0)
+
+countdown = 5 # in seconds
+decimate = 10
+duration = 10 # in seconds
+fps = 20. # in Hz, should be just slightly higher than the actual FPS of the video
+
+## Count down
+for i in range(countdown,0,-1):
+    print(i)
+    time.sleep(1)
+
+## Find the active zone
+print('Localizing the camera display.')
+
+# Find pixels that change
+previous_image = imageio.imread('<screen>') #[::decimate, ::decimate]
+zone = np.zeros(previous_image.shape, dtype = bool)
+for i in range(10):
+    print(i)
+    time.sleep(.1)
+    image = imageio.imread('<screen>') #[::decimate, ::decimate]
+    zone = zone | (image != previous_image)
+
+# Find columns and rows
+columns = zone.sum(axis=0).nonzero()[0]
+x1, x2 = columns[0], columns[-1]+1
+rows = zone.sum(axis=1).nonzero()[0]
+y1, y2 = rows[0], rows[-1]+1
+
+## Recording
+t0 = t = time.time()
+i = 0
+while t<t0+duration:
+    image = imageio.imread('<screen>')[y1:y2, x1:x2]
+    ## Saving
+    imageio.imsave("tif/test{}.png".format(i), image)
+
+    new_t = time.time()
+    if new_t-t < 1/fps:
+        time.sleep(1/fps - new_t-t)
+    else:
+        print("lost frame",i,"by ",new_t-t - 1/fps,"second")
+    t = time.time()
+    i += 1
