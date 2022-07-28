0cf9e93f4929e5b7cca8319fb6af1cb231a27272
Marcel Stimberg <marcel.stimberg@inserm.fr>
2022-07-28 22:32:50 +0200

    Move crop area to track Paramecium

--- yolo_gui.py
+++ yolo_gui.py
@@ -23,6 +23,10 @@ class TrackerConfig(Config):
     crop_y = Integer(848, bounds=(0, None), doc='y of cropping area')
     crop_width = Integer(416, bounds=(0, None), doc='width of cropping area')
     crop_height = Integer(352, bounds=(0, None), doc='height of cropping area')
+    only_show_cropped = Boolean(default=False, doc='Only show cropped video?')
+
+    move_stage = Boolean(default=False, doc='Move stage to track?')
+    max_displacement = Integer(default=10, bounds=(0, None), doc='Max displacement in pixel')
 
     weights = Filename('/home/marcel/programming/Paramecium-deeplearning/best.pt', doc='Filename for trained weights')
 
@@ -30,7 +34,8 @@ class TrackerConfig(Config):
     iou_threshold = Number(0.45, bounds=(0, 1), doc='IoU threshold')
     max_detections = Integer(1000, bounds=(0, None), doc='Maximum detections')
 
-    categories = [('Image', ['crop', 'crop_x', 'crop_y', 'crop_width', 'crop_height']),
+    categories = [('Image', ['crop', 'crop_x', 'crop_y', 'crop_width', 'crop_height', 'only_show_cropped']),
+                  ('Movement', ['move_stage', 'max_displacement']),
                   ('Network', ['weights']),
                   ('Parameters', ['conf_threshold', 'iou_threshold', 'max_detections'])
                   ]
@@ -103,7 +108,7 @@ class YoloTracker():
 
         if boxes.size:
             dist_to_center = np.sqrt((boxes[:, 0] + boxes[:, 2]/2 - 0.5)**2 + (boxes[:, 1] + boxes[:, 3]/2 - 0.5)**2)
-            confidence = np.array([p[4] for p in pred[0]])
+            confidence = np.array([p[4] for p in pred[0].cpu()])
             meta_data = {'dist_to_center': dist_to_center, 'confidence': confidence, 'crop_rect': crop_rect}
         else:
             meta_data = {'dist_to_center': np.array([]), 'confidence': np.array([]), 'crop_rect': crop_rect}
@@ -111,16 +116,35 @@ class YoloTracker():
 
     def receive_image(self, image):
         if not self.do_track:
-            return image
+            if self.config.only_show_cropped:
+                print(self.config.crop_y, self.config.crop_x, self.config.crop_width, self.config.crop_height)
+                return np.array(image[self.config.crop_y:self.config.crop_y+self.config.crop_height,
+                                      self.config.crop_x:self.config.crop_x+self.config.crop_width,
+                                      :])
+            else:
+                return image
         import time
         start = time.time()
         self.detections, self.metadata = self.detect(image)
+        if len(self.detections) and self.config.move_stage:
+            distances = self.metadata['dist_to_center']
+            min_idx = np.argmin(distances)
+            # Move crop area so that the object is in the center
+            move_x = min(int((self.detections[min_idx, 0] - 0.5) * self.config.crop_width), self.config.max_displacement)
+            self.config.crop_x = int(np.clip(self.config.crop_x + move_x, 0, image.shape[1]-self.config.crop_width))
+            move_y = min(int((self.detections[min_idx, 1] - 0.5) * self.config.crop_height), self.config.max_displacement)
+            self.config.crop_y = int(np.clip(self.config.crop_y + move_y, 0, image.shape[0]-self.config.crop_height))
+        
         took = time.time() - start
         print('Detected {} Paramecia in {:.2f}s (confidence: {})'.format(len(self.detections),
                                                                          took,
                                                                          ', '.join('{:.2f}'.format(c) for c in self.metadata['confidence'])))
-
-        return image
+        if self.config.only_show_cropped:
+            return np.array(image[self.config.crop_y:self.config.crop_y+self.config.crop_height,
+                                  self.config.crop_x:self.config.crop_x+self.config.crop_width,
+                                  :])
+        else:
+            return image
 
     def mark_cells(self, pixmap):
         if not self.do_track:
@@ -128,7 +152,7 @@ class YoloTracker():
         from PyQt5 import QtGui, QtCore
 
         painter = QtGui.QPainter(pixmap)
-        if self.metadata.get('crop_rect', None) is not None:
+        if self.metadata.get('crop_rect', None) is not None and not self.config.only_show_cropped:
             x, y, w, h = self.metadata['crop_rect']
             # show cropping area
             pen = QtGui.QPen(QtGui.QColor(0, 0, 0, 50))
